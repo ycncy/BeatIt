e78ab6cadc5f4c406e2cc8a6173c7d818a2f3f73
Talhaoui <yacine.talhaoui@etu.univ-amu.fr>
2022-02-28 15:09:58 +0100

    Ah maintenant c'est un peu mieux (GUI)

--- src/main/java/app/foodapp/controller/FoodAppController.java
+++ src/main/java/app/foodapp/controller/FoodAppController.java
@@ -1,10 +1,18 @@
 package app.foodapp.controller;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import recipe.FavoriteRecipe;
 import recipe.JsonReader;
+import recipe.Recipe;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -18,21 +26,48 @@ public class FoodAppController implements Initializable {
     @FXML private Spinner spinner;
     @FXML private TextField ingredientTextField;
     @FXML private Hyperlink recipeName;
-    @FXML private Text text;
-    @FXML private Text textTest;
-    @FXML private Label label;
+    @FXML private Label ingredientsText;
+    @FXML private VBox vboxRecipesCenter;
+    @FXML private VBox vboxFavorite;
 
-    public void initialize(URL location, ResourceBundle resourceBundle) {}
+    public void initialize(URL location, ResourceBundle resourceBundle) {
+        for(Recipe recipe : FavoriteRecipe.getRecipes()) {
+            HBox hbox = new HBox();
+            hbox.setLayoutX(10.0);
+            hbox.setPrefHeight(25.0);
+            hbox.setPrefWidth(244.0);
+            hbox.setPadding(new Insets(10,10,10,10));
+
+            Hyperlink hyperlink = new Hyperlink();
+            Button button = new Button();
+
+            hyperlink.setText(recipe.getName());
+            hyperlink.setPrefHeight(32.0);
+            hyperlink.setPrefWidth(156.0);
+            hyperlink.setId(recipe.getName());
+
+            button.setText("-");
+            button.setMnemonicParsing(false);
+            button.setPrefHeight(31.0);
+            button.setPrefWidth(31.0);
+            button.setId("deleteFromFavorite");
+
+            hbox.getChildren().add(hyperlink);
+            hbox.getChildren().add(button);
+
+            vboxFavorite.getChildren().add(hbox);
+        }
+    }
 
     @FXML
     private void addIngredient() {
         if(ingredientTextField.getText() != null) {
             if(JsonReader.allIngredients.contains(ingredientTextField.getText())) {
                 JsonReader.ingredients.add(ingredientTextField.getText());
-                if (text.getText().equals("")) {
-                    text.setText(ingredientTextField.getText());
+                if (ingredientsText.getText().equals("")) {
+                    ingredientsText.setText(ingredientTextField.getText());
                 } else {
-                    text.setText(text.getText() + ", " + ingredientTextField.getText());
+                    ingredientsText.setText(ingredientsText.getText() + ", " + ingredientTextField.getText());
                 }
                 ingredientTextField.clear();
             } else {
@@ -46,8 +81,72 @@ public class FoodAppController implements Initializable {
     }
 
     @FXML
-    private void test() {
-        JsonReader.setRecipes(String.valueOf(5));
-        text.setText(JsonReader.recipes.toString());
+    private void displayRecipes() {
+
+        JsonReader.setRecipes(String.valueOf(30));
+        for(Recipe recipe : JsonReader.recipes) {
+            //HBOX
+            HBox hbox = new HBox();
+            hbox.setPrefHeight(0.0);
+            hbox.setPrefWidth(517);
+            hbox.setPadding(new Insets(10,10,10,10));
+
+            //HyperLink et Button
+            Hyperlink hyperlink = new Hyperlink();
+            Button button = new Button();
+            hyperlink.setText(recipe.getName());
+            hyperlink.setPrefHeight(0.0);
+            hyperlink.setContentDisplay(ContentDisplay.RIGHT);
+            hyperlink.setPrefWidth(530);
+            hyperlink.setId("recipeName");
+            button.setAlignment(Pos.CENTER);
+            button.setMnemonicParsing(false);
+            button.setPrefHeight(31.0);
+            button.setPrefWidth(31.0);
+            button.setId("addToFavorite");
+            button.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    FavoriteRecipe.addFavorite(recipe);
+                    if(!FavoriteRecipe.getRecipes().contains(recipe)) {
+                        HBox hbox = new HBox();
+                        hbox.setLayoutX(10.0);
+                        hbox.setPrefHeight(25.0);
+                        hbox.setPrefWidth(244.0);
+                        hbox.setPadding(new Insets(10,10,10,10));
+
+                        Hyperlink hyperlink = new Hyperlink();
+                        Button button = new Button();
+
+                        hyperlink.setText(recipe.getName());
+                        hyperlink.setPrefHeight(32.0);
+                        hyperlink.setPrefWidth(156);
+                        hyperlink.setId("recipeName");
+                        button.setText("-");
+
+                        button.setMnemonicParsing(false);
+                        button.setPrefHeight(31.0);
+                        button.setPrefWidth(31.0);
+                        button.setId("deleteFromFavorite");
+
+                        hbox.getChildren().add(hyperlink);
+                        hbox.getChildren().add(button);
+
+                        vboxFavorite.getChildren().add(hbox);
+                    } else {
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                        alert.setTitle("Ingredient ERROR");
+                        alert.setHeaderText("Déjà dans les favoris");
+                        alert.showAndWait();
+                        ingredientTextField.clear();
+                    }
+                }
+            });
+
+            //Ajout des deux
+            hbox.getChildren().add(hyperlink);
+            hbox.getChildren().add(button);
+            vboxRecipesCenter.getChildren().add(hbox);
+        }
     }
 }
